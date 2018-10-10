b82208846d25d49b6cee45072bbde6f8fea0ae85
clydedacruz <clydecroix@gmail.com>
2018-10-11 00:52:00 +0530

    blockstack signin, signup, getfile, putfile working

--- App.js
+++ App.js
@@ -1,31 +1,104 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, { Component } from 'react';
+import {NativeModules} from 'react-native';
+import {
+  StyleSheet,
+  Button,
+  Text,
+  View
+} from 'react-native';
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+blockstack = NativeModules.BlockstackNativeModule;
+const textFileName = "message.txt"
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+export default class App extends Component {
 
-type Props = {};
-export default class App extends Component<Props> {
+    constructor(props) {
+        super(props);
+        this.state = {
+            loaded: false, 
+            userData: null,
+            fileContents: null,
+            fileUrl: null
+        };
+      }
+
+    componentDidMount() {
+        this.createSession()
+    }
+
   render() {
-    return (
+    if (this.state.userData) {
+      signInText = "Signed in as " + this.state.userData.decentralizedID
+    } else {
+      signInText = "Not signed in"
+    }
+
+    return (      
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text tyle={styles.welcome}>Blockstack React Native Example</Text>  
+        
+        <Button title="Sign In with Blockstack" onPress={() => this.signIn()}
+        disabled = {!this.state.loaded || this.state.userData != null}/>
+        <Text>{signInText}</Text>
+
+        <Button title="Sign out" onPress={() => this.signOut()}
+        disabled = {!this.state.loaded || this.state.userData == null}/>
+        
+        <Button title="Put file" onPress={() => this.putFile()}
+        disabled = {!this.state.loaded || this.state.userData == null}/>
+        <Text>{this.state.fileUrl}</Text>
+        
+        <Button title="Get file" onPress={() => this.getFile()}
+        disabled = {!this.state.loaded || this.state.userData == null}/>
+        <Text>{this.state.fileContents}</Text>
       </View>
     );
+  }
+
+  async createSession() {
+    config = {
+      appDomain:"https://flamboyant-darwin-d11c17.netlify.com",
+      scopes:["store_write"]
+    }
+    result = await blockstack.createSession(config)
+
+    console.log("created " + result["loaded"])
+    this.setState({loaded:result["loaded"]})
+  }
+
+  async signIn() {
+    console.log("signIn")
+    console.log("current state: " + JSON.stringify(this.state))
+    result = await blockstack.signIn();
+    
+    console.log("result: " + JSON.stringify(result))
+    this.setState({userData:{decentralizedID:result["decentralizedID"]}})
+  }
+
+  async signOut() {
+    result = await blockstack.signUserOut()
+
+    console.log(JSON.stringify(result))
+    if (result["signedOut"]) {
+      this.setState({userData: null})
+    }
+  }
+
+  async putFile() {
+    this.setState({fileUrl: "uploading..."})
+    content = "Hello React Native"
+    options = {encrypt: false}
+    result = await blockstack.putFile(textFileName, content, options)
+    console.log(JSON.stringify(result))
+    this.setState({fileUrl: result["fileUrl"]})
+  }
+
+  async getFile() {
+    this.setState({fileContents: "downloading..."})
+    options = {decrypt:false}
+    result = await blockstack.getFile(textFileName, options)
+    console.log(JSON.stringify(result))
+    this.setState({fileContents: result["fileContents"]})
   }
 }
 
